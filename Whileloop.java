171009c891c4d2a2fa0dbc9e02acc2e81b35ffb2
Mohamed Abi Vakkas <abimass784@gmail.com>
2025-08-24 01:58:31 +0530

    Add files via upload

---
+++ Whileloop.java
@@ -0,0 +1,28 @@
+package com.dev;
+
+public class Whileloop {
+	public static void main(String[] args)
+	{
+		int i=2,sum=0;
+//		while(i<=10)
+//		{
+//			sum=sum+i;
+//			i++;
+//			
+//		}
+//		System.out.println(sum);
+//		while(i>=1)
+//		{
+//			System.out.println(i);
+//			i--;
+//			
+//		}
+//		System.out.println("Blast");
+		while(i%2==0) {//odd number sums
+			sum=sum+i;
+			i++;
+			
+		}
+		
+	}
+}
