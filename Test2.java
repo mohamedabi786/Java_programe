b3f8f2d872ae5b821982b1118e95822c1625b0a3
Mohamed Abi Vakkas <abimass784@gmail.com>
2025-08-30 23:06:56 +0530

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,173 @@
+package com.test;
+
+import java.util.Scanner;
+
+public class Test2 {
+	
+//			 static int f(int n)
+//			{
+//				return n*n;
+//			}
+//	enum WeekDay {
+//	    MON, TUE, WED, THURS, FRI, SAT, SUN
+//	}
+
+//		
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+//		System.out.println("Enter the number:");
+//		int m=sc.nextInt();
+//		int re;
+//		for(int i=1;i<=10;i++)
+//		{
+//			re=m*i;
+//			System.out.println();
+//			System.out.println(i+"*"+m+"="+re);
+//		}
+//		System.out.println("Enter the first number:");
+//		int f=sc.nextInt();
+//		System.out.println("Enter the Secound number:");
+//		int s=sc.nextInt();
+//		String result= (f>s) ? f+" "+"is big" : s+" "+"is big";
+//		System.out.println(result);
+//		System.out.println("Enter the first number:");
+//		int f=sc.nextInt();
+//		System.out.println("Enter the Secound number:");
+//		int s=sc.nextInt();
+//		System.out.println("Enter the third number:");
+//		int t=sc.nextInt();
+//		int lar;
+//		int sec;
+//		if(f>s && f>t)
+//		{
+//			lar=f;
+//			sec=(s>t)? s : t;
+//		
+//		}
+//		else if(s>f && s>t)
+//		{
+//			lar=s;
+//			sec=(f>t)? f : t;
+//		}
+//		else {
+//			lar=t;
+//			sec=(f>s)? f : s;
+//		}
+//		System.out.println(sec+" "+"is Secound large number.");
+//		System.out.println("\n...Student Grade...");
+//		System.out.println("Enter your tamil mark:");
+//		int t=sc.nextInt();
+//		System.out.println("Enter your english mark:");
+//		int e=sc.nextInt();
+//		System.out.println("Enter your maths mark:");
+//		int m=sc.nextInt();
+//		System.out.println("Enter your java mark:");
+//		int j=sc.nextInt();
+//		System.out.println("Enter your python mark:");
+//		int p=sc.nextInt();
+//		int total=t+e+m+j+p;
+//		double avg=total/5.0;
+//		 if (avg >= 90) {
+//	            System.out.println("Grade: O");
+//	        } else if (avg >= 80) {
+//	            System.out.println("Grade: A");
+//	        } else if (avg >= 70) {
+//	            System.out.println("Grade: B");
+//	        } else if (avg >= 60) {
+//	            System.out.println("Grade: C");
+//	        } else {
+//	            System.out.println("Grade: Fail");
+//	        }
+//		System.out.println("Square a Number using metho");
+//		System.out.println("Enter a number:");
+//		int num=sc.nextInt();
+//		int result=f(num);
+//		System.out.println(result);
+//		System.out.println("SWICH FOR OPERATION");
+//		System.out.print("Enter first number: ");
+//	    int num1 = sc.nextInt();
+//	    System.out.print("Enter second number: ");
+//        int num2 = sc.nextInt();
+//        System.out.print("Enter operator (+, -, *, /, %): ");
+//        char ch = sc.next().charAt(0);
+//        int result = 0;
+//        switch (ch) {
+//        case '+':
+//            result = num1 + num2;
+//            break;
+//        case '-':
+//            result = num1 - num2;
+//            break;
+//        case '*':
+//            result = num1 * num2;
+//            break;
+//        case '/':
+//            if (num2 == 0) {
+//                System.out.println("Error: Division by zero!");
+//                return;
+//            }
+//            result = num1 / num2;
+//            break;
+//        case '%':
+//            result = num1 % num2;
+//            break;
+//        default:
+//            System.out.println("Invalid operator!");
+//            return;
+//    }
+//        System.out.println("Result: " + result);
+//
+//		       
+//		System.out.println("Day using enum");
+//		System.out.println("Enter a number (1-7): ");
+//	    int num = sc.nextInt();
+//	    if (num >= 1 && num <= 7) {
+//	      WeekDay day = WeekDay.values()[num - 1]; 
+//	      System.out.println("Day is: " + day);
+//	    }
+//		System.out.println("Sum of prime  number");
+//		  int sum = 0;
+//
+//	        for (int num = 2; num <= 100; num++) {
+//	            boolean isPrime = true;
+//
+//	            // Check prime
+//	            for (int i = 2; i <= num / 2; i++) {
+//	                if (num % i == 0) {
+//	                    isPrime = false;
+//	                    break;
+//	                }
+//	            }
+//
+//	            if (isPrime) {
+//	                sum += num; // add prime number
+//	            }
+//	        }
+//
+//	        System.out.println("Sum of prime numbers from 1 to 100 = " + sum);
+//		System.out.println("Sum of even and odd number");
+//		  int evenSum = 0;
+//	        int oddSum = 0;
+//
+//	        // Let’s take numbers from 1 to 100
+//	        for (int i = 1; i <= 100; i++) {
+//	            if (i % 2 == 0) {
+//	                evenSum += i; // add even numbers
+//	            } else {
+//	                oddSum += i; // add odd numbers
+//	            }
+//	        }
+//
+//	        System.out.println("Sum of Even Numbers: " + evenSum);
+//	        System.out.println("Sum of Odd Numbers: " + oddSum);
+//		       
+		
+
+	        
+		
+		
+	}
+
+	
+}
